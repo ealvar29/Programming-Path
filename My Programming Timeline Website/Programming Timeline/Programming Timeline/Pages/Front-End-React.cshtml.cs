41185d4d2a5e89b6e8d253c6db3a8664ae26648e
Eduardo <edalvarez2015@gmail.com>
2020-10-01 20:40:08 -0500

    Entire project commit

---
+++ My Programming Timeline Website/Programming Timeline/Programming Timeline/Pages/Front-End-React.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Programming_Timeline.Pages
+{
+    public class ReactModel : PageModel
+    {
+        private readonly ILogger<ReactModel> _logger;
+
+        public ReactModel(ILogger<ReactModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
